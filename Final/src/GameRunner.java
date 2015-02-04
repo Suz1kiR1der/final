150f08258901a241208e06ebf0cb977b9a869c4f
Christopher.Ferguson <Christopher.Ferguson@MU-C3-21.mullen.pvt.k12.co.us>
2015-02-04 10:17:01 -0700

    Initial commit

---
+++ Final/src/GameRunner.java
@@ -0,0 +1,11 @@
+
+public class GameRunner
+	{
+
+		public static void main(String[] args)
+			{
+				PlayerMaker.greetUser();
+				
+			}
+
+	}
---
+++ Final/src/UserPlayer.java
@@ -0,0 +1,11 @@
+
+public class UserPlayer
+	{
+
+		public static void main(String[] args)
+			{
+				
+				
+			}
+
+	}
