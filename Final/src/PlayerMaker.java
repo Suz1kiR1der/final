3e4709707248e05933edb2fd3cff5aa85104fbd6
CPF <CPF@192.168.1.16>
2015-02-09 17:59:52 -0700

    Finished with comPlayerMaker, now working on player Skill

--- Final/src/PlayerMaker.java
+++ Final/src/PlayerMaker.java
@@ -258,7 +258,7 @@ public class PlayerMaker
 			}
 		public static void comPlayerMaker()
 			{
-				int com = (int)(Math.random()* 37)+1;
+				int com = (int)(Math.random()* 188)+1;
 				if(com == 1)
 					{
 						System.out.println("And your enemies are: Kobald, Dark Wizard, and a Dragon.");
@@ -407,5 +407,617 @@ public class PlayerMaker
 					{
 						System.out.println("And your enemies are: Dark Wizard, Dragon, and a Death Knight.");
 					}
+				if(com == 38)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Giant.");
+				}
+				if(com == 39)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Goblin.");
+				}
+				if(com == 40)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and an Orc.");
+				}
+				if(com == 41)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and the Beholder.");
+				}
+				if(com == 42)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Vampire King.");
+				}
+				if(com == 43)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Vampire Queen.");
+				}
+				if(com == 44)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and Alduin.");
+				}
+				if(com == 45)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Griffin.");
+				}
+				if(com == 46)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and an Ogre.");
+				}
+				if(com == 47)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and Nezznar the Black Spider.");
+				}
+				if(com == 48)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and Mormesk the Wraith.");
+				}
+				if(com == 49)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Gargoyle.");
+				}
+				if(com == 49)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Wyvern.");
+				}
+				if(com == 50)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Stone Giant.");
+				}
+				if(com == 51)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Stone Golem.");
+				}
+				if(com == 52)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Troll.");
+				}
+				if(com == 53)
+				{
+					System.out.println("And your enemies are: Dragon, Gelatin Cube, and a Death Knight.");
+				}
+				if(com == 54)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Goblin.");
+				}
+				if(com == 55)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and an Orc.");
+				}
+				if(com == 55)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and the Beholder.");
+				}
+				if(com == 56)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Vampire King.");
+				}
+				if(com == 57)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Vampire Queen.");
+				}
+				if(com == 58)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and Alduin.");
+				}
+				if(com == 59)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Griffin.");
+				}
+				if(com == 60)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and an Ogre.");
+				}
+				if(com == 61)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and Nezznar the Black Spider.");
+				}
+				if(com == 62)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and Mormesk the Wraith.");
+				}
+				if(com == 63)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Gargoyle.");
+				}
+				if(com == 64)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Wyvern.");
+				}
+				if(com == 65)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Stone Giant.");
+				}
+				if(com == 66)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Stone Golem.");
+				}
+				if(com == 67)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Troll.");
+				}
+				if(com == 68)
+				{
+					System.out.println("And your enemies are: Gelatin Cube, Giant, and a Death Knight.");
+				}
+				if(com == 69)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and an Orc.");
+				}
+				if(com == 70)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and the Beholder.");
+				}
+				if(com == 71)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Vampire King.");
+				}
+				if(com == 72)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Vampire Queen.");
+				}
+				if(com == 73)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and Alduin.");
+				}
+				if(com == 74)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Griffin.");
+				}
+				if(com == 75)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and an Ogre.");
+				}
+				if(com == 76)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and Nezznar the Black Spider.");
+				}
+				if(com == 77)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and Mormesk the Wraith.");
+				}
+				if(com == 78)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Gargoyle.");
+				}
+				if(com == 79)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Wyvern.");
+				}
+				if(com == 80)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Stone Giant.");
+				}
+				if(com == 81)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Stone Golem.");
+				}
+				if(com == 82)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Troll.");
+				}
+				if(com == 83)
+				{
+					System.out.println("And your enemies are: Giant, Goblin, and a Death Knight.");
+				}
+				if(com == 84)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and the Beholder.");
+				}
+				if(com == 85)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Vampire King.");
+				}
+				if(com == 86)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Vampire Queen.");
+				}
+				if(com == 87)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and Alduin.");
+				}
+				if(com == 88)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Griffin.");
+				}
+				if(com == 89)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and an Ogre.");
+				}
+				if(com == 90)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and Nezznar the Black Spider.");
+				}
+				if(com == 91)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and Mormesk the Wraith.");
+				}
+				if(com == 92)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Gargoyle.");
+				}
+				if(com == 93)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Wyvern.");
+				}
+				if(com == 94)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Stone Giant.");
+				}
+				if(com == 95)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Stone Golem.");
+				}
+				if(com == 96)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Troll.");
+				}
+				if(com == 97)
+				{
+					System.out.println("And your enemies are: Goblin, Orc, and a Death Knight.");
+				}
+				if(com == 98)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Vampire King.");
+				}
+				if(com == 99)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Vampire Queen.");
+				}
+				if(com == 100)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and Alduin.");
+				}
+				if(com == 101)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Griffin.");
+				}
+				if(com == 102)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and an Ogre.");
+				}
+				if(com == 103)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and Nezznar the Black Spider.");
+				}
+				if(com == 104)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and Mormesk the Wraith.");
+				}
+				if(com == 105)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Gargoyle.");
+				}
+				if(com == 106)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Wyvern.");
+				}
+				if(com == 107)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Stone Giant.");
+				}
+				if(com == 108)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Stone Golem.");
+				}
+				if(com == 109)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Troll.");
+				}
+				if(com == 110)
+				{
+					System.out.println("And your enemies are: Orc, the Beholder, and a Death Knight.");
+				}
+				if(com == 111)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Vampire Queen.");
+				}
+				if(com == 112)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and Alduin.");
+				}
+				if(com == 113)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Griffin.");
+				}
+				if(com == 114)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and an Ogre.");
+				}
+				if(com == 115)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and Nezznar the Black Spider.");
+				}
+				if(com == 116)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and Mormesk the Wraith.");
+				}
+				if(com == 117)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Gargoyle.");
+				}
+				if(com == 118)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Wyvern.");
+				}
+				if(com == 119)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Stone Giant.");
+				}
+				if(com == 120)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Stone Golem.");
+				}
+				if(com == 121)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Troll.");
+				}
+				if(com == 122)
+				{
+					System.out.println("And your enemies are: The Beholder, Vampire King, and a Death Knight.");
+				}
+				if(com == 123)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and Alduin.");
+				}
+				if(com == 124)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Griffin.");
+				}
+				if(com == 125)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and an Ogre.");
+				}
+				if(com == 126)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and Nezznar the Black Spider.");
+				}
+				if(com == 127)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and Mormesk the Wraith.");
+				}
+				if(com == 128)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Gargoyle.");
+				}
+				if(com == 129)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Wyvern.");
+				}
+				if(com == 130)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Stone Giant.");
+				}
+				if(com == 131)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Stone Golem.");
+				}
+				if(com == 132)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Troll.");
+				}
+				if(com == 133)
+				{
+					System.out.println("And your enemies are: Vampire King, Vampire Queen, and a Death Knight.");
+				}
+				if(com == 134)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Griffin.");
+				}
+				if(com == 135)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and an Ogre.");
+				}
+				if(com == 136)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and Nezznar the Black Spider.");
+				}
+				if(com == 137)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and Mormesk the Wraith.");
+				}
+				if(com == 138)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Gargoyle.");
+				}
+				if(com == 139)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Wyvern.");
+				}
+				if(com == 140)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Stone Giant.");
+				}
+				if(com == 141)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Stone Golem.");
+				}
+				if(com == 142)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Troll.");
+				}
+				if(com == 143)
+				{
+					System.out.println("And your enemies are: Vampire Queen, Alduin, and a Death Knight.");
+				}
+				if(com == 144)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and an Ogre.");
+				}
+				if(com == 145)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and Nezznar the Black Spider.");
+				}
+				if(com == 146)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and Mormesk the Wraith.");
+				}
+				if(com == 147)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Gargoyle.");
+				}
+				if(com == 148)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Wyvern.");
+				}
+				if(com == 149)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Stone Giant.");
+				}
+				if(com == 150)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Stone Golem.");
+				}
+				if(com == 151)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Troll.");
+				}
+				if(com == 152)
+				{
+					System.out.println("And your enemies are: Alduin, Griffin, and a Death Knight.");
+				}
+				if(com == 153)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and Nezznar the Black Spider.");
+				}
+				if(com == 154)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and Mormesk the Wraith.");
+				}
+				if(com == 155)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Gargoyle.");
+				}
+				if(com == 156)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Wyvern.");
+				}
+				if(com == 157)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Stone Giant.");
+				}
+				if(com == 158)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Stone Golem.");
+				}
+				if(com == 159)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Troll.");
+				}
+				if(com == 160)
+				{
+					System.out.println("And your enemies are: Griffin, Ogre, and a Death Knight.");
+				}
+				if(com == 161)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and Mormesk the Wraith.");
+				}
+				if(com == 162)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and a Gargoyle.");
+				}
+				if(com == 163)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and a Wyvern.");
+				}
+				if(com == 164)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and a Stone Giant.");
+				}
+				if(com == 165)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and Stone Golem.");
+				}
+				if(com == 166)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and a Troll.");
+				}
+				if(com == 167)
+				{
+					System.out.println("And your enemies are Ogre, Nezznar the Black Spider, and a Death Knight.");
+				}
+				if(com == 168)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Gargoyle.");
+				}
+				if(com == 169)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Wyvern.");
+				}
+				if(com == 170)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Stone Giant.");
+				}
+				if(com == 171)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Stone Golem.");
+				}
+				if(com == 172)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Troll.");
+				}
+				if(com == 173)
+				{
+					System.out.println("And your enemies are Nezznar the Black Spider, Mormesk the Wraith, and a Death Knight.");
+				}
+				if(com == 174)
+				{
+					System.out.println("And your enemies are: Mormesk the Wraith, Gargoyle, and a Wyvern.");
+				}
+				if(com == 175)
+				{
+					System.out.println("And your enemies are: Mormesk the Wraith, Gargoyle, and a Stone Giant.");
+				}
+				if(com == 176)
+				{
+					System.out.println("And your enemies are: Mormesk the Wraith, Gargoyle, and a Stone Golem.");
+				}
+				if(com == 177)
+				{
+					System.out.println("And your enemies are: Mormesk the Wraith, Gargoyle, and a Troll.");
+				}
+				if(com == 178)
+				{
+					System.out.println("And your enemies are: Mormesk the Wraith, Gargoyle, and a Death Knight.");
+				}
+				if(com == 179)
+				{
+					System.out.println("And your enemies are: Gargoyle, Wyvern, and a Stone Giant.");
+				}
+				if(com == 180)
+				{
+					System.out.println("And your enemies are: Gargoyle, Wyvern, and a Stone Golem.");
+				}
+				if(com == 181)
+				{
+					System.out.println("And your enemies are: Gargoyle, Wyvern, and a Troll.");
+				}
+				if(com == 182)
+				{
+					System.out.println("And your enemies are: Gargoyle, Wyvern, and a Death Knight.");
+				}
+				if(com == 183)
+				{
+					System.out.println("And your enemies are: Wyvern, Stone Giant, and a Stone Golem.");
+				}
+				if(com == 184)
+				{
+					System.out.println("And your enemies are: Wyvern, Stone Giant, and a Troll.");
+				}
+				if(com == 185)
+				{
+					System.out.println("And your enemies are: Wyvern, Stone Giant, and a Death Knight.");
+				}
+				if(com == 186)
+				{
+					System.out.println("And your enemies are: Stone Giant, Stone Golem, and a Troll.");
+				}
+				if(com == 187)
+				{
+					System.out.println("And your enemies are: Stone Giant, Stone Golem, and a Death Knight.");
+				}
+				if(com == 188)
+				{
+					System.out.println("And your enemies are: Stone Golem, Troll, and a Death Knight.");
+				}
 			}
 	}
